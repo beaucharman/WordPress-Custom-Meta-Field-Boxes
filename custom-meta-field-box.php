7d850a8a12d7092c7d1ebc11d13243b95c4f52f9
Beau Charman <beau.charman@gmail.com>
2013-05-03 08:01:10 +1000

    core changes

--- custom-meta-field-box.php
+++ custom-meta-field-box.php
@@ -5,14 +5,14 @@
  * custom-meta-field-box.php
  * @version 2.1 | May 1st 2013
  * @author  Beau Charman | @beaucharman | http://beaucharman.me
- * @link    https://github.com/beaucharman/wordpress-custom-meta-field-box
+ * @link    https://github.com/beaucharman/wordpress-custom-meta-field-boxes
  * @license MIT license
  *
  * To declare a custom meta field box, simply create a new instance of the
  * LT3_Custom_Meta_Field_Box class.
  *
  * Configuration guide:
- * https://github.com/beaucharman/wordpress-custom-meta-field-box
+ * https://github.com/beaucharman/wordpress-custom-meta-field-boxes
  *
  * For more information about registering custom meta field boxes:
  * http://codex.wordpress.org/Function_Reference/add_meta_box
